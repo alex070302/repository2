9af1d6b195ba3a5373da777694cf0aab42c512ec
unknown <alessiomon02@gmail.com>
2021-09-03 11:59:35 +0200

    modifica effettuata

--- arrayesercizio.php
+++ arrayesercizio.php
@@ -1,11 +1,34 @@
 <?php
-$numeri=[20,30,70,40,15];
-print_r ($numeri);
-echo ("<br>");
-echo max($numeri);
-echo ("<br>");
-echo min($numeri);
+function trovaNumeroMaggiore(){
+$numeri=[115,30,90,80,110];
+$j=0;
+$numeroMaggiore=0;
+for ($i=0;$i<count($numeri);$i++){
 
-
+    if($numeri[$j]<$numeri[$i]){
+   $numeri[$j]=$numeri[$i];
+}
+$numeroMaggiore=$numeri[$j];
+   
+}
+echo ("il numero maggiore è " . $numeroMaggiore);
+}
+function trovaNumeroMinore(){
+    $numeri=[130,30,5,20,110];
+    $j=0;
+    $numeroMinore=0;
+    for ($i=0;$i<count($numeri);$i++){
+    
+        if($numeri[$j]>$numeri[$i]){
+       $numeri[$j]=$numeri[$i];
+    }
+    $numeroMinore=$numeri[$j];
+       
+    }
+    echo ( "il numero maggiore è " . $numeroMinore);
+    }
+    trovaNumeroMaggiore();
+    echo "<br>";
+    trovaNumeroMinore();
 
 ?>
